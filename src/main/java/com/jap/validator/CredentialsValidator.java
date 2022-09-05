655532ad23f87e397fab36b7e6687b5822e0c79a
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-09-05 16:35:49 +0530

    pc1

--- src/main/java/com/jap/validator/CredentialsValidator.java
+++ src/main/java/com/jap/validator/CredentialsValidator.java
@@ -1,5 +1,6 @@
 package com.jap.validator;
 
 public interface CredentialsValidator {
+    String validator(String name,String password);
 
 }
--- src/main/java/com/jap/validator/CredentialsValidatorImpl.java
+++ src/main/java/com/jap/validator/CredentialsValidatorImpl.java
@@ -2,7 +2,16 @@ package com.jap.validator;
 
 public class CredentialsValidatorImpl {
     public String validateUser(String name, String password){
-        return "";
+        CredentialsValidator credentialsValidator=(n1,n2)->{
+            if (n1.equals("admin")&& n2.equals("1234")){
+                return "valid user";
+            }else {
+                return "invalid user";
+            }
+        };
+
+
+        return credentialsValidator.validator(name, password);
     }
 
 
